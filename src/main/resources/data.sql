0f876d2b39571beb06791cf983d3c295de3ecc79
dmden22 <maslovden22@yandex.ru>
2023-12-08 14:33:05 +0300

    Edit application.properties & data.sql
    To fill in the database data when running on a PC
    without a schema.
    (drop table if exist, create table, insert data).

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,41 @@
+drop table if exists users_roles, users, roles;
+
+create table if not exists users
+(
+    id       bigint auto_increment primary key,
+    username varchar(255) not null unique,
+    password varchar(255) not null
+);
+
+create table if not exists roles
+(
+    id   bigint primary key auto_increment,
+    name varchar(255) unique
+);
+
+insert into roles(name)
+values ('ROLE_USER');
+insert into roles(name)
+values ('ROLE_ADMIN');
+
+create table if not exists users_roles
+(
+    user_id bigint,
+    role_id bigint,
+    primary key (user_id, role_id)
+);
+
 INSERT INTO users (username, password)
-VALUES ('user', '$2y$10$X3hj3qZ1nb3ok6.CBK1Tu.J0UShodeFkC7S4gKEAd7ZfipFVjJq.q'),
-       ('admin', '$2y$10$nR2NSC8mf1XQyoNAskL85OfNIxATrBVgwjEbbOJ1O1Fte0xrzNkLK');
+VALUES ('user', '$2y$10$X3hj3qZ1nb3ok6.CBK1Tu.J0UShodeFkC7S4gKEAd7ZfipFVjJq.q');
+INSERT INTO users (username, password)
+VALUES ('admin', '$2y$10$nR2NSC8mf1XQyoNAskL85OfNIxATrBVgwjEbbOJ1O1Fte0xrzNkLK');
 
-INSERT INTO roles (name)
-VALUES ('ROLE_USER'),
-       ('ROLE_ADMIN');
+alter table users_roles
+    add foreign key (user_id) references users (id);
+alter table users_roles
+    add foreign key (role_id) references roles (id);
 
-INSERT INTO users_roles (user_id, role_id)
-VALUES ('1', '1'),
-       ('2', '2');
+insert into users_roles (user_id, role_id)
+VALUES (1, 1);
+insert into users_roles (user_id, role_id)
+VALUES (2, 2);
